43024a72dc5f4342cd346303a336a0f57fcf4e7a
rmfttl <chogj27@gmail.com>
2026-01-15 17:56:08 +0900

    열번째 커밋

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace TextRPG
+{
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MainGame mainGame = new MainGame();
+
+            mainGame.Initialize();
+            mainGame.Progress();
+
+
+        }
+    }
+}
